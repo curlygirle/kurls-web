2f66b3f7b870aa0f42dd9738a0b2c57b568dc5ef
Erin Harper <erinharper24@gmail.com>
2024-10-24 22:00:04 +0200

    Some Changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,23 +7,47 @@ export default function About() {
     <section>
       <main className="aboutus">
         <h2>about us</h2>
-        <p cl>
-          Nostrud enim pariatur culpa amet excepteur eiusmod nulla sint. Do eu
-          cupidatat eu culpa minim veniam ex occaecat reprehenderit. Dolore eu
-          id eiusmod amet officia amet ad culpa duis aliquip. Labore
-          reprehenderit consectetur pariatur proident mollit incididunt velit
-          pariatur occaecat.
+        <p>
+          Welcome to Kurls, your go-to destination for all things natural hair
+          care! We believe that every curl, coil, and wave deserve to be
+          celebrated, nurtured, and embraced. Our mission is to empower
+          individuals with textured hair to love and care for their hair with
+          confidence, by providing personalized solutions, expert guidance, and
+          community support.
+        </p>
+        <p>
+          At Kurls, we understand that no two heads of hair are the same. That’s
+          why our approach is all about customization and education. From our
+          in-depth hair questionnaires that help you identify your unique hair
+          type and needs, to our curated product recommendations, we’re here to
+          make your hair journey smoother and more rewarding.{" "}
+        </p>
+        <p>
+          Our platform is designed to offer more than just hair care advice.
+          It’s a space for community, learning, and self-expression. Discover
+          styling tutorials, ingredient glossaries, hair growth tracking, and
+          expert insights—all tailored to help you achieve your hair goals.
+          Whether you’re a seasoned naturalist or just beginning your journey,
+          Kurls is here to support you every step of the way.
         </p>
         <img src={placeholder} alt="" />
       </main>
       <main className="ourfounder">
         <h2>our founder</h2>
         <p>
-          Nostrud enim pariatur culpa amet excepteur eiusmod nulla sint. Do eu
-          cupidatat eu culpa minim veniam ex occaecat reprehenderit. Dolore eu
-          id eiusmod amet officia amet ad culpa duis aliquip. Labore
-          reprehenderit consectetur pariatur proident mollit incididunt velit
-          pariatur occaecat.
+          I, Erin Harper, the visionary behind Kurls, am passionate about
+          celebrating natural beauty and empowering people to embrace their
+          unique hair textures. As someone who has personally navigated the
+          challenges of finding the right products and routines for natural
+          hair, I understand the importance of a personalized, informed approach
+          to hair care.
+        </p>
+        <p>
+          Dedicated to fostering diversity and inclusion, I am committed to
+          making hair care accessible to everyone. Whether you're looking to
+          enhance your natural hair routine, discover new products, or simply
+          connect with others who share similar hair journeys, Kurls is here to
+          inspire and support you every step of the way.
         </p>
         <img src={placeholder} alt="" />
       </main>
--- src/pages/Hair Details/TypeGuide.js
+++ src/pages/Hair Details/TypeGuide.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "../../styles/TypeGuide.css";
+import placeholder from "../../images/placeholder.jpeg";
 
 export default function TypeGuide() {
   const hairTypes = [
-    { type: "straight", characteristics: "sleek, shiny, difficult to curl" },
-    { type: "wavy", characteristics: "s-shaped waves, prone to frizz" },
-    { type: "curly", characteristics: "defined curls, springy texture" },
-    { type: "coily", characteristics: "tight coils, very fragile" },
+    {
+      type: "straight",
+      characteristics: "sleek, shiny, difficult to curl",
+      image: placeholder,
+    },
+    {
+      type: "wavy",
+      characteristics: "s-shaped waves, prone to frizz",
+      image: placeholder,
+    },
+    {
+      type: "curly",
+      characteristics: "defined curls, springy texture",
+      image: placeholder,
+    },
+    {
+      type: "coily",
+      characteristics: "tight coils, very fragile",
+      image: placeholder,
+    },
   ];
 
   return (
@@ -23,6 +40,9 @@ export default function TypeGuide() {
                 <p className="type-des">
                   characteristics: {hairType.characteristics}
                 </p>
+                <section className="type-image-container">
+                  <img className="type-image" src={hairType.image} alt="" />
+                </section>
               </section>
             ))}
           </section>
