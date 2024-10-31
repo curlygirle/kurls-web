605aa3543284bd7f97029d6c55388dcb8858c981
Erin Harper <erinharper24@gmail.com>
2024-10-31 22:26:37 +0200

    Some Changes

--- src/pages/Product Details/ProductPage.js
+++ src/pages/Product Details/ProductPage.js
@@ -5,26 +5,28 @@ import "../../styles/ProductPages.css";
 
 export default function ProductPage() {
   return (
-    <section className="card-container">
-      {hairProducts.map((product) => (
-        <section key={product.id} className="card">
-          <img src={placeholder} alt={product.name} className="card-img" />
-          <section className="card-details">
-            <h3 className="card-title">{product.name}</h3>
-            <section className="card-price">
-              <section className="price">
-                {product.onSale ? (
-                  <>
-                    <del>R{product.price}</del> R{product.salePrice}
-                  </>
-                ) : (
-                  `R${product.price}`
-                )}
+    <section className="page-wrapper">
+      <section className="card-container">
+        {hairProducts.map((product) => (
+          <section key={product.id} className="card">
+            <img src={placeholder} alt={product.name} className="card-img" />
+            <section className="card-details">
+              <h3 className="card-title">{product.name}</h3>
+              <section className="card-price">
+                <section className="price">
+                  {product.onSale ? (
+                    <>
+                      <del>R{product.price}</del> R{product.salePrice}
+                    </>
+                  ) : (
+                    `R${product.price}`
+                  )}
+                </section>
               </section>
             </section>
           </section>
-        </section>
-      ))}
+        ))}
+      </section>
     </section>
   );
 }
