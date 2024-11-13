2837e6890148adcbb6bb0a093914c5bc0c46c755
Erin Harper <erinharper24@gmail.com>
2024-11-13 22:23:32 +0200

    Styling Done

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -6,7 +6,7 @@ import ScrollToTop from "../functions/ScrollToTop";
 export default function Community() {
   return (
     <section className="blog">
-      <h2 className="main-title">latest from the blogs</h2>
+      <h2 className="main-title">recommended blogs</h2>
       <section className="blog-container">
         {blogPosts.map((post) => (
           <section key={post.id} className="blog-card">
