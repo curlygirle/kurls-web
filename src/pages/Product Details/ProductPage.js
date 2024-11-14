757becc9d7a703cb706f4629791da125032d2953
Erin Harper <erinharper24@gmail.com>
2024-11-14 02:43:37 +0200

    Changes

--- src/pages/Product Details/ProductPage.js
+++ src/pages/Product Details/ProductPage.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { hairProducts } from "../../components/ProductData";
 import "../../styles/ProductPages.css";
 
 export default function ProductPage() {
+  //catergory filtering system
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const categories = [
+    "All",
+    ...new Set(hairProducts.map((product) => product.category)),
+  ];
+
+  // filter products based on selected category
+  const filteredProducts =
+    selectedCategory === "All"
+      ? hairProducts
+      : hairProducts.filter((product) => product.category === selectedCategory);
+
   return (
     <section className="page-wrapper">
+      <section className="filter-container">
+        {categories.map((category) => (
+          <button
+            key={category}
+            onClick={() => setSelectedCategory(category)}
+            className={`filter-button ${
+              selectedCategory === category ? "filter-button-active" : ""
+            }`}
+          >
+            {category}
+          </button>
+        ))}
+      </section>
       <section className="card-container">
-        {hairProducts.map((product) => (
+        {filteredProducts.map((product) => (
           <section key={product.id} className="card">
             <img src={product.image} alt={product.name} className="card-img" />
             <section className="card-details">
